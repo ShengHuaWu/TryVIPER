eba1bffccd63bb2a7825568ea1cccc9779f3d959
ShengHua Wu <fantasy0404@gmail.com>
2017-09-18 19:05:10 +0800

    Create ImageDetailInteractor.

---
+++ TryVIPER/Modules/ImageDetail/ImageDetailInteractor.swift
@@ -0,0 +1,25 @@
+//
+//  ImageDetailInteractor.swift
+//  TryVIPER
+//
+//  Created by ShengHua Wu on 18/09/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Image Detail Interactor Output
+protocol ImageDetailInteractorOutput: class {
+    func endDownloadingImage(to url: URL)
+}
+
+// MARK: - Image Detail Interactor
+final class ImageDetailInteractor {
+    // MARK: Properties
+    weak var output: ImageDetailInteractorOutput?
+    
+    // MARK: Public Methods
+    func downloadImage(with imageProvider: ImageProvider = ImageProvider()) {
+        // TODO: Download image and invoke output
+    }
+}
